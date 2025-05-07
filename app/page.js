47ef78f30b8c926e4008ff56b338dada0ededbda
Nextorcinus <nextruangkerja@gmail.com>
2025-05-07 17:26:46 +0700

    fix: perbaiki struktur halaman utama / dengan 'use client'

---
+++ app/page.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Home() {
+  return (
+    <main className="p-6 text-white">
+      <h1 className="text-2xl font-bold">Special Lazyness</h1>
+      <p>Welcome to the upgrade calculator!</p>
+    </main>
+  )
+}
