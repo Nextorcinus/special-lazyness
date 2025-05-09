65f07cf94ae91051dedff0226f2a677dc5a8bbbb
Nextorcinus <nextruangkerja@gmail.com>
2025-05-09 15:03:23 +0700

    page lainnya

--- app/dashboard/research/page.js
+++ app/dashboard/research/page.js
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="p-6 text-white">
-      <h1 className="text-2xl font-bold">Special Lazyness Home Page</h1>
+    <div className="p-6 text-center text-zinc-500 h-screen content-center">
+      <h1 className="text-2xl ">Research Page is under construction.</h1>
     </div>
   )
 }
