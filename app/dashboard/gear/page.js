4c6481476bd233ac6fa5e43078b242f44b4f7e3d
Nextorcinus <nextruangkerja@gmail.com>
2025-05-09 15:11:27 +0700

    perbaikan tinggi

--- app/dashboard/gear/page.js
+++ app/dashboard/gear/page.js
@@ -2,7 +2,7 @@
 
 export default function GearPage() {
   return (
-    <div className="p-6 text-center text-zinc-500 h-screen content-center">
+    <div className="p-6 text-center text-zinc-500 h-full content-center">
       <h1 className="text-2xl ">Gear Page is under construction.</h1>
     </div>
   )
--- app/dashboard/research/page.js
+++ app/dashboard/research/page.js
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="p-6 text-center text-zinc-500 h-screen content-center">
+    <div className="p-6 text-center text-zinc-500 h-full content-center">
       <h1 className="text-2xl ">Research Page is under construction.</h1>
     </div>
   )
