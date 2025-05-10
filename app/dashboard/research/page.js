a8d32d6c6904b876eefa47490964d81fafd7c184
Ryouta <nextruangkerja@gmail.com>
2025-05-10 16:34:51 +0700

    perbaikan

--- app/dashboard/research/page.js
+++ app/dashboard/research/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="p-6 text-center text-zinc-500 h-full content-center">
-      <h1 className="text-xl text-lime-500 lg:text-4xl  py-5">
+      <h1 className="text-xl text-zinc-400 lg:text-4xl  py-5">
         Research Page is under construction.
       </h1>
       <p>I'll continue when laziness takes over again. '_'</p>
