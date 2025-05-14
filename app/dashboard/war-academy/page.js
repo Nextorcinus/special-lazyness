adb2df593ebae2de7bb493d376dfd16e53cfbb1e
Nextorcinus <nextruangkerja@gmail.com>
2025-05-14 10:20:26 +0700

    update path

--- app/dashboard/war-academy/page.js
+++ app/dashboard/war-academy/page.js
@@ -12,7 +12,7 @@ import HeliosTotalResult from '../../components/HeliosTotalResult'
 import ResourceIcon from '../../components/ResourceIcon'
 import { formatToShortNumber } from '../../utils/formatToShortNumber'
 
-import heliosData from '../../data/HelliosResearch.json'
+import heliosData from '../../data/HeliosResearch.json'
 import { useHeliosHistory } from './HeliosHistoryContext'
 import { useAddAnother } from '../../dashboard/research/AddAnotherContext'
 
