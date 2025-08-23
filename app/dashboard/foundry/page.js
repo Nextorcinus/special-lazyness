d0e45aaaf856768a85e77aacd5d6b2b53c3ba8bc
Ryouta <nextruangkerja@gmail.com>
2025-08-24 04:06:53 +0700

    fix some path

--- app/dashboard/foundry/page.js
+++ app/dashboard/foundry/page.js
@@ -1,9 +1,9 @@
 'use client'
-import TaskBoard from '../components/TaskBoard' 
+import TaskBoard from '../../components/TaskBoard'
 
 export default function FoundryPage() {
   return (
-      <main className="p-4 md:p-6 text-white w-full">
+    <main className="p-4 md:p-6 text-white w-full">
       <div className="relative mb-6 bg-special-inside border border-zinc-800 rounded-2xl p-6 shadow-md">
         <h2 className="text-2xl text-white">R4 / R5 Assign Task</h2>
       </div>
