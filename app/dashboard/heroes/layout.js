e6a85f5c1818360d6c824b69e338495661fa5ac8
Ryouta <nextruangkerja@gmail.com>
2025-11-24 21:39:09 +0700

    update mobile

--- app/dashboard/heroes/layout.js
+++ app/dashboard/heroes/layout.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function HeroesLayout({ children }) {
   return (
-    <main className="w-full flex justify-center text-white">
-      <div className="w-full md:max-w-[765px] xl:max-w-[1200px] mt-0 md:mt-12 px-4 md:px-6 lg:px-8">
+    <main
+      className="w-full flex justify-center text-white"
+      style={{ touchAction: 'pan-y' }}
+    >
+      <div className="w-full md:max-w-[765px] xl:max-w-[1200px] px-4 md:px-6 lg:px-8">
         <div className="py-2">
           {children}
         </div>
