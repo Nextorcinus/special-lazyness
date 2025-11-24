184c83c62b1371f11b9bf9aed5f4634a5398676e
Ryouta <nextruangkerja@gmail.com>
2025-11-24 21:22:42 +0700

    update

--- app/dashboard/heroes/layout.js
+++ app/dashboard/heroes/layout.js
@@ -2,14 +2,12 @@ import React from 'react'
 
 export default function HeroesLayout({ children }) {
   return (
-    <div className="w-full flex justify-center text-white">
-      <main className="w-full flex justify-center">
-        <div className="w-full md:max-w-[765px] xl:max-w-[1200px] px-4 md:px-6 lg:px-8 mt-0 md:mt-8">
-          <div className="py-2">
-            {children}
-          </div>
+    <main className="w-full flex justify-center text-white">
+      <div className="w-full md:max-w-[765px] xl:max-w-[1200px] mt-0 md:mt-12 px-4 md:px-6 lg:px-8">
+        <div className="py-2">
+          {children}
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   )
 }
