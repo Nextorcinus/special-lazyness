d1779f124726d9b6caff2d238e490dbed6da69c0
Ryouta <nextruangkerja@gmail.com>
2025-11-26 23:10:04 +0700

    update hero gen s10 without pic

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="max-w-2xl text-center">
         <h1 className="text-4xl text-teal-300">Special Lazyness</h1>
         <p className="text-white mt-4">
-          I made this appe because I am lazy. Seriously. I hate calculating things. Numbers stress me out. So I created this tool to make life easier not smarter just easier.
+          I made this app because I am lazy. Seriously. I hate calculating things. Numbers stress me out. So I created this tool to make life easier, not smarter just easier.
 
           Enjoy it. Use it. Let it do the thinking while you do absolutely anything else.
           special_one 👻
